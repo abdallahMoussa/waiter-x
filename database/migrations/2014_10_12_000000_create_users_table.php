c47431875bcb37d24581ab174571b52eb8eeac39
abdallah.moussa2017@outlook.com <abdallah.moussa2017@outlook.com>
2023-08-15 23:51:36 +0300

    commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('phone');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('content');
             $table->boolean('subscribed');
             $table->timestamp('email_verified_at')->nullable();
